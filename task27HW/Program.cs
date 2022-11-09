34e52828ac54022f7871bf8ac847296d0f5d0e49
Наталья Ярцева <zabaluna@mail.ru>
2022-11-10 00:28:36 +0300

    Summery of print number

---
+++ task27HW/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Задача 27: Напишите программу, которая принимает на вход число 
+//  и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+void GetSumNums(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= Math.Abs(number); i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"Cумма цифр введенного числа: {sum}");
+}
+GetSumNums(n);
