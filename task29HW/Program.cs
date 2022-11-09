029e4b949c6aae1a23e01b4c469f51ed246e465a
Наталья Ярцева <zabaluna@mail.ru>
2022-11-10 00:06:32 +0300

    Random massive N

---
+++ task29HW/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Задача 29: Напишите программу, которая задаёт массив из N 
+//  элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = GetBinaryArray(n);
+
+Console.WriteLine($" [ {String.Join(",", array)} ] ");
+
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0,n);
+    }
+
+    return result;
+}
+
